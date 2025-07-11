75f063a6bbc46c7982fa330651720175b3eb65a3
Naga Aishwarya <nagaahari111@gmail.com>
2025-07-11 19:57:11 +0530

    Add files via upload

---
+++ EvenOddChecker.py
@@ -0,0 +1,11 @@
+#Even or Odd
+
+# List of numbers
+numbers = [10, 23, 45, 66, 77, 90]
+
+# Loop through the list and check each number
+for num in numbers:
+    if num % 2 == 0:
+        print(f"{num} is Even")
+    else:
+        print(f"{num} is Odd")
